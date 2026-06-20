8633f6c6d1b232c757c8d60abd85659179405d35
trst3385 <trst3385@naver.com>
2026-06-20 15:59:46 +0900

    feat: TreeSlot 추가 배치 및 배치 로직 개선
    - 맵 내 TreeSlot 배치 지점 4곳으로 확장.
    - isTreeSelected 상태 변수를 도입하여, 나무 선택 전 배치 방지.
    - PlacementManager.cs에 배치 모드 종료 로직 구현하여 다른 TreeSlot에 중복 배치 차단.
     1. 배치 후, 선택 모드를 종료해 다른 TreeSlot 클릭시 선택하지 않은 상태에서도 나무가 배치되는 문제 해결

--- Assets/Scripts/Managers/PlacementManager.cs
+++ Assets/Scripts/Managers/PlacementManager.cs
@@ -6,6 +6,7 @@ public class PlacementManager : MonoBehaviour
     public GameObject[] treePrefabs;//여러 나무를 인스펙터에서 리스트로 관리
     public int currentSelectedTreeIndex = 0;//지금 선택된 나무 인덱스
 
+    private bool isTreeSelected = false;//나무를 선택했는지 확인하는 상태 변수
 
 
     void Update()
@@ -22,6 +23,11 @@ public class PlacementManager : MonoBehaviour
              //왜 써야 해?: 이게 없으면 특정 나무를 선택하려고 UI 버튼을 눌렀을 뿐인데,
              //맵 뒤에 있는 다른 오브젝트나 UI, TreeSlot 등. 같이 클릭될 수 있는 '클릭 관통(Click-through)' 현상이 발생해.
 
+            if (!isTreeSelected)//나무가 선택되지 않았다면 아래 로직을 실행하지 않고 중단
+            {
+                Debug.Log("나무를 먼저 선택해!");
+                return;
+            }
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//카메라에서 마우스 위치로 레이 발사
             RaycastHit hit;
@@ -36,9 +42,13 @@ public class PlacementManager : MonoBehaviour
                     if (treePrefabs[currentSelectedTreeIndex] == null)//인덱스가 비었을때. 빨간색 오류로 띄우기 (문제가 있거나 심각할 때)
                     {
                         Debug.LogError("[시스템 오류] 배치할 나무 프리팹이 연결되지 않았어! 인스펙터를 확인해!");
+                        return;//오류 시 함수 종료
                     }
 
                     slot.PlantTree(treePrefabs[currentSelectedTreeIndex]);//현재 선택된 나무를 배치
+
+                    isTreeSelected = false;//배치 완료 후 선택 해제
+                    Debug.Log("나무 배치 완료! 다음 treeSlot에 배치를 할때 다시 나무를 선택해줘!");
                 }
             }
         }
@@ -48,6 +58,7 @@ public class PlacementManager : MonoBehaviour
     {
         //버튼에서 들어온 인덱스로 현재 선택된 나무를 바꿈
         currentSelectedTreeIndex = index;
+        isTreeSelected = true;//버튼을 누르면 선택된 상태로 변경
         Debug.Log($"{treePrefabs[currentSelectedTreeIndex].name}을(를) 선택했어!");
-    }  
+    }
 }

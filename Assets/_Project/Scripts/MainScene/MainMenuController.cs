248e72ef9f5a909848725faedc4b768b2ec7606f
trst3385 <trst3385@naver.com>
2026-08-12 14:21:22 +0900

    feat: 메인화면 씬 및 UI 컨트롤러 연결 구현
    - 메인화면(MainScene) 생성 및 Build Settings 등록
    - MainMenuController 스크립트 작성 (게임 시작/종료 로직 구현).
    - MainMenuController 오브젝트를 하이어라키에 배치하여 OnClick 이벤트 연결 완료.
    - 씬/스크립트 폴더 구조 정리.
     1. 프로젝트 폴더 내 Assets > _Project > (작품의 에셋, 스크립트폴더)
     2. Scripts 폴더 내에 메인화면 씬 관련 스크립트를 모은 MainScene 폴더

---
+++ Assets/_Project/Scripts/MainScene/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void LoadGameScene()//함수 자체에 씬 이름을 고정
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()//게임 종료 버튼에 연결할 함수
+    {
+        Debug.Log("게임 종료!");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
